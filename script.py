8bdb654e75110bf05429c39a1489e4c999ba788b
pepo999 <127298097+pepo999@users.noreply.github.com>
2023-11-02 12:59:29 +0100

    added bible.txt

--- script.py
+++ script.py
@@ -11,8 +11,7 @@ collection = db['wiki']
 wiki_list = list(collection.find({}, {"_id": 0, "title": 0}))
 wiki_text = [x['text']for x in wiki_list]
 
-# reut sent:  [['ASIAN', 'EXPORTERS', 'FEAR', 'DAMAGE', 'FROM', 'U', '.', 'S', '.-', 'JAPAN', 'RIFT', 'Mounting', 
-# 'trade', 'friction', 'between', 'the', 'U', '.', 'S', '.', 'And', 'Japan', 'has', 'raised', 'fears', 'among', 'many', 'of', 'Asia', "'", 's', 'exporting', 'nations', 'that', 'the', 'row', 'could', 'inflict', 'far', '-', 'reaching', 'economic', 'damage', ',', 'businessmen', 'and', 'officials', 'said', '.'], ['They', 'told', 'Reuter', 'correspondents', 'in', 'Asian', 'capitals', 'a', 'U', '.', 'S', '.', 'Move', 'against', 'Japan', 'might', 'boost', 'protectionist', 'sentiment', 'in', 'the', 'U', '.', 'S', '.', 'And', 'lead', 'to', 'curbs', 'on', 'American', 'imports', 'of', 'their', 'products', '.'], ...]
+
 
 wiki_data = []
 for text in wiki_text:
@@ -34,7 +33,6 @@ for w1_w2 in model:
     for w3 in model[w1_w2]:
         model[w1_w2][w3] /= total_count
  
-
 def sentence(a, b, counter): 
     if counter == 10:
         return
@@ -50,16 +48,20 @@ def sentence(a, b, counter):
                 text.append(word)
                 break
         if text[-2:] == [None, None]:
-            sentence_finished = True       
-    print (' '.join([t for t in text if t]))
-    time.sleep(3)
-    first_word = text[0]
-    next_word = text[1] 
-    if next_word is None:
-        next_word = 'world'
+            sentence_finished = True    
+    sentence_res =  ' '.join([t for t in text if t]) 
+    print(sentence_res.capitalize())
+    time.sleep(1)
+    random_position = random.randint(0, len(text) - 1)
+    first_word = text[random_position - 1]
+    next_word = text[random_position] 
     if first_word is None:
-        first_word = 'the'
+        return
+    if next_word is None:
+        next_word = text[-1]
+        # if text[-1] is None:
+        #     return
     sentence(first_word, next_word, counter)
     
 counter = 0
-sentence("to", "the", counter)
+sentence("the", "world", counter)
--- wiki_scrape.py
+++ wiki_scrape.py
@@ -2,13 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 import random
 from pymongo import MongoClient
+import re
+import time
 
 client = MongoClient('mongodb+srv://pietroviglino999:rkoEZiZp6tzduEUZ@vpp4dbtest.yseft60.mongodb.net/admin?retryWrites=true&replicaSet=atlas-du9683-shard-0&readPreference=primary&srvServiceName=mongodb&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1', 27017)
 db = client['language_model']
 collection = db['wiki']
-
-titles_db = list(collection.find({}, {"_id":0, "text": 0}))
-titles_list = [x['title'] for x in titles_db]
 
 def scrapeWikiArticle(url):
     try:
@@ -16,30 +15,35 @@ def scrapeWikiArticle(url):
         soup = BeautifulSoup(response.content, 'html.parser')
         heading = soup.find(id="firstHeading")
         title = heading.text
-        if title not in titles_list and title != 'Web scraping':   
+        titles_db = list(collection.find({}, {"_id":0, "text": 0}))
+        titles_list = [x['title'] for x in titles_db]
+        if title not in titles_list and title != 'Web scraping' and  ':' not in title:   
             parafs = soup.find_all("p", recursive=True)
             text = []
             for p in parafs:
                 p_content = p.text
                 p_content = p_content.replace('\n', '')
-                p_list = p_content.split('.')
+                p_content = re.sub(r'\[[^\]]*\]', '', p_content)
+                p_content = re.sub(r'\([^)]*\)', '', p_content)
+                # p_list = re.split(r'(?<=[^A-Z]{2})\.\s', p_content)
+                p_list = re.split(r'(?<![A-Z])\.\s', p_content)
+                # p_list = p_content.split('. ')
                 for sentence in p_list:
                     if sentence != '':
+                        if sentence[-1] != '.':
+                            sentence += '.'
                         text.append(sentence)
             collection.insert_one({"title": title, "text": text})
             print('added doc on db about ', title)
         allLinks = soup.find(id="bodyContent").find_all("a")
         random.shuffle(allLinks)
         linkToScrape = 0
-
         for link in allLinks:
-            # We are only interested in other wiki articles
             if link['href'].find("/wiki/") == -1: 
                 continue
-
-            # Use this link to scrape
             linkToScrape = link
             break
+        time.sleep(1)
         scrapeWikiArticle("https://en.wikipedia.org" + linkToScrape['href'])
     except Exception as e:
         print(e)
